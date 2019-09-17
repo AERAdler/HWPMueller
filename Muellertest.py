c8af649b8fb2aeae6892b9d54e678c929fc3259e
AERAdler <alexandre.adler@mapadler.ch>
2019-09-17 16:52:05 +0200

    Added phi and transmisson calc

--- Muellertest.py
+++ Muellertest.py
@@ -3,3 +3,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+eps_1 = 0+0j
+eps_2 = 0+0j
+
+Celerity = 3e8 #Speed of light in medium
+
+sapphire_transmission = np.loadtxt('dummy_sapphire.txt')
+
+def transmission(nu):
+
+    return [ np.interp(nu, sapphire_transmission[:,0], sapphire_transmission[:,1]), 
+    np.interp(nu, sapphire_transmission[:,0], sapphire_transmission[:,2]) ]
+
+def phi(nu, n_s, n_f, d):
+
+    return 2.0*np.pi*d*(n_s-n_f)*nu
+
